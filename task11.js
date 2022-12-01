f8874aac9e65fada5ef43926792bd758e2956c05
Satya2008 <satyanand807@gmail.com>
2022-12-01 19:49:04 +0530

    some init msg

---
+++ task11.js
@@ -0,0 +1,27 @@
+1. find is it prime or not?
+let num=13;
+let count=0;
+for(let i=0; i>=13; i++)
+{
+if(num%2==0)
+{
+count++
+}
+}
+if(count==2){
+console.log("it is prime num")
+}
+else{
+console.log("it is not prime")
+}
+2. Given string is palindrome or not?
+let str="masai";
+let sum=0;
+for(let i=; i<=str.length; i++){
+bag+=str[i];
+}
+if(str[i]==bag){
+console.log("It is palindrome")
+}else{
+console.log("It is not palindrome")
+}
